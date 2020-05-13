73448a73f87d286e1b40050c254946e4fca53d56
Ulises <umartinezadon@outlook.com>
2020-05-13 15:24:05 -0400

    Added some changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Ulises Martinez Adon',                   # Type in your name
   author_email = 'umartinezadon@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/umartinez22',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/umartinez22/archive/v_1.0.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/umartinez22/python-huawei-cloud-messaging/archive/v_1.0.1.tar.gz',    # I explain this later on
   keywords = ['Huawei', 'cloud', 'messaging', 'notification'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
@@ -16,6 +16,6 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.8.2'
+    'Programming Language :: Python'
   ],
 )
