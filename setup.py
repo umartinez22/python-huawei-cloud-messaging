d33376583cc75360da49bfedb674ba7f93a445ba
Ulises <umartinezadon@outlook.com>
2020-05-13 15:55:07 -0400

    Changed folder structure

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from distutils.core import setup
 
-version = '1.0.2'
+version = '1.0.3'
 
 setup(
-  name = 'pyhcm',         # How you named your package folder (MyLib)
-  packages = ['pyhcm'],   # Chose the same as "name"
-  version = f'{version}',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Wrapper of api push huawei for sending notification push using python.',   # Give a short description about your library
-  author = 'Ulises Martinez Adon',                   # Type in your name
-  author_email = 'umartinezadon@outlook.com',      # Type in your E-Mail
-  url = 'https://github.com/umartinez22',   # Provide either the link to your github or to your website
-  download_url = f'https://github.com/umartinez22/python-huawei-cloud-messaging/archive/v_{version}.tar.gz',    # I explain this later on
-  keywords = ['Huawei', 'cloud', 'messaging', 'notification'],   # Keywords that define your package best
-  install_requires=[],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python'
-  ],
+    name='pyhcm',
+    packages=['pyhcm'],
+    version=f'{version}', license='MIT',
+    description='Wrapper of api push huawei for sending notification push using python.',
+    author='Ulises Martinez Adon',
+    author_email='umartinezadon@outlook.com',
+    url='https://github.com/umartinez22',
+    download_url=f'https://github.com/umartinez22/python-huawei-cloud-messaging/archive/v_{version}.tar.gz',
+    keywords=['Huawei', 'cloud', 'messaging', 'notification'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python'
+    ],
 )
